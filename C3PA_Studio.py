065f6028f55fc35f4f6bfb39bdf9c7d8d93b53b4
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-03-03 12:27:46 -0800

    Add files via upload

---
+++ C3PA_Studio.py
@@ -0,0 +1,15 @@
+# [2502] n8
+#
+
+import streamlit as st
+
+st.title("C3PA Studio")
+
+st.write("Tools to work with C3PA data assets.")
+st.write("Select an option to the left to drill down to a specific topic.")
+st.write("Link to CCC-Bot")
+st.write("Link to the corresponding repo")
+st.write("Number of test questions logged : 40")
+
+st.sidebar.success("Select an option above.")
+
