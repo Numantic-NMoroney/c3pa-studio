225d6481159d0b5f23e97cbff2a373dd998bce0c
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-03-03 13:03:27 -0800

    Add files via upload

--- C3PA_Studio.py
+++ C3PA_Studio.py
@@ -9,7 +9,7 @@ st.write("Tools to work with C3PA data assets.")
 st.write("Select an option to the left to drill down to a specific topic.")
 st.write("Link to CCC-Bot")
 st.write("Link to the corresponding repo")
-st.write("Number of test questions logged : 40")
+st.write("Number of test questions logged : 0")
 
 st.sidebar.success("Select an option above.")
 
