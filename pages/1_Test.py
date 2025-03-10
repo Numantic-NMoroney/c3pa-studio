84bf5e2a7efcb7e6f501acf739530b87cfcace36
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-03-10 10:27:16 -0700

    Add files via upload

--- pages/1_Test.py
+++ pages/1_Test.py
@@ -9,6 +9,10 @@ import time
 import hashlib
 
 
+sys.path.insert(0, "rag")
+sys.path.insert(0, "utils")
+from rag_bot import CCCPolicyAssistant
+
 def portable_hash(string):
     encoded_string = string.encode('utf-8')
     hash_object = hashlib.sha256(encoded_string)
