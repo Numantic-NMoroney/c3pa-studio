40eecc7db35aeafa4ce0bfd77dbbc41813925aae
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-03-10 10:11:21 -0700

    Add files via upload

--- C3PA_Studio.py
+++ C3PA_Studio.py
@@ -8,8 +8,8 @@ testing_path = os.environ.get('MOUNT_PATH', '/c3pa-app/testing')    # gcs
 # testing_path='./c3pa-app/testing'
 
 
-def get_test_count():
-    count_path = os.path.join(testing_path, "test_count.txt")
+def get_count(name_txt):
+    count_path = os.path.join(testing_path, name_txt)
     test_count = ''
     with open(count_path, "r") as file:
         test_count = file.readline()
@@ -23,7 +23,8 @@ st.write("Select an option to the left to drill down to a specific topic.")
 st.write("Link to CCC-Bot")
 st.write("Link to the corresponding repo")
 
-st.write("Number of test questions logged : " + get_test_count())
+st.write("Number of test questions logged : " + get_count("test_count.txt"))
+st.write("Number of uaser questions answered : " + get_count("user_question_count.txt"))
 
 st.sidebar.success("Select an option above.")
 
