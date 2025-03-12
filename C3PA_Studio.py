428e42e176bab0925f4e945895ef8ccc750bdaeb
Numantic-NMoroney <nathan.moroney@numanticsolutions.com>
2025-03-12 16:37:25 -0700

    Add files via upload

--- C3PA_Studio.py
+++ C3PA_Studio.py
@@ -2,32 +2,11 @@
 #
 
 import streamlit as st
-import json, os, sys
+import os, sys
 
+sys.path.insert(0, "utils")
 
-def configure_app():
-    sys.path.insert(0, "data")
-    with open("data/c3pa_studio_path.json") as file:
-        mount_path = json.load(file)['mount_path']
-
-    has_term = os.environ.get('TERM') == 'xterm-256color'
-    if has_term:
-        app_path = './c3pa-app/testing'
-    else:
-        app_path = os.environ.get('MOUNT_PATH', mount_path)
-
-    with open(app_path + "/c3pa_studio_config.json") as file:
-        cfg = json.load(file)
-
-    return mount_path, app_path, cfg
-
-
-def get_count(name_txt):
-    count_path = os.path.join(app_path, name_txt)
-    test_count = ''
-    with open(count_path, "r") as file:
-        test_count = file.readline()
-    return test_count        
+from utility_functions import get_count, configure_app
 
 
 mount_path, app_path, cfg = configure_app()
@@ -42,11 +21,15 @@ st.write("Link to CCC-Bot")
 st.write("Link to the corresponding repo")
 
 st.write("C3PA Studio version : " + cfg['version'])
-st.write("Number of test questions logged : " + get_count("test_count.txt"))
-st.write("Number of user questions answered : " + get_count("user_question_count.txt"))
 
-st.sidebar.success("Select an option above.")
+test_count = get_count(app_path, "test_count.txt")
+st.write("Number of test questions logged : " + test_count)
+
+user_question_count = get_count(app_path, "user_question_count.txt")
+st.write("Number of user questions answered : " + user_question_count)
 
 term = os.environ.get('TERM')
 st.write("term : " + str(term))
+
+st.sidebar.success("Select an option above.")
 
